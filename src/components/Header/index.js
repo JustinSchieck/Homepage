b890671d24996075c6485c55adabce911dd917e3
Justin Schieck <schieck91@gmail.com>
2020-01-26 17:35:28 -0500

    intial push 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const RootDiv = styled.div`
+    height: 500px;
+    background-color: grey;
+`;
+
+const Image = styled.img`
+    max-height: 200px;
+`;
+
 const Header = () => {
-    return (<div>this is a header</div>);
+    return (<RootDiv>
+        <Image src="../me.png" alt="portfolio" />
+    </RootDiv>);
 };
 
 export default Header;
