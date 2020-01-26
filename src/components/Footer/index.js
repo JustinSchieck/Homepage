976ba8955aaa61f1b1bc816cb19f425bd2213c44
Justin Schieck <schieck91@gmail.com>
2020-01-26 00:47:25 -0500

    initial push

---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+    return (<div>this is a footer</div>);
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (<div>this is a header</div>);
+};
+
+export default Header;
