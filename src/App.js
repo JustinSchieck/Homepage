c80d659b59059e194010128e86d1db7070eaac80
Justin Schieck <justin_schieck@manulife.ca>
2020-08-05 10:29:06 -0400

    pushing upstream to work on personal computer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
 import React from "react";
 import styled from 'styled-components';
-import "./App.css";
+import {
+  TransitionGroup,
+  CSSTransition
+} from "react-transition-group";
+import { ArrowForward, ArrowBack } from "@material-ui/icons";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link,
+  Redirect,
+  useLocation,
+  useParams
 } from "react-router-dom";
-import AnimatedNav from './components/AnimatedNav';
+import Homepage from './containers/Homepage';
+import AboutPage from "./containers/AboutPage";
+
+const RootDiv = styled.div`
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+`;
+
+const Overlay = styled.div`
+  z-index: 1;
+  position: fixed;
+  right: 50px;
+  top: 45vh;
+  a > div {
+    text-decoration: none;
+    color: black !important;
+  }
+`;
+
+const ArrowRight = styled(ArrowForward)`
+  position: fixed;
+  right: 50px;
+  top: 45vh;
+`;
+
+const ArrowLeft = styled(ArrowBack)`
+  position: fixed;
+  left: 50px;
+  top: 45vh;
+`;
+
+const RouteLink = styled(Link)`
+  color: black;
+`;
+
+const routes = [
+  { path: '/', name: 'Home', Component: Homepage },
+  { path: '/about', name: 'About', Component: AboutPage }
+]
+
 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="*">
-          <AnimatedNav />
-        </Route>
-      </Switch>
+      <RootDiv>
+        <Overlay>
+          <RouteLink to="/about">
+            <ArrowRight
+              style={{ marginTop: "8px", fontSize: "3rem" }}
+            />
+          </RouteLink>
+          <RouteLink to="/">
+            <ArrowLeft
+              style={{ marginTop: "8px", fontSize: "3rem" }}
+            />
+          </RouteLink>
+        </Overlay>
+        <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </RootDiv>
     </Router>
   );
 }
--- src/components/AnimatedNav/index.js
+++ src/components/AnimatedNav/index.js
@@ -19,6 +19,12 @@ import AboutPage from "../../containers/AboutPage";
 
 const RootDiv = styled.div`
   height: 100vh;
+  
+`;
+
+const RouteDiv = styled.div`
+  position: relative;
+  
 `;
 
 const Overlay = styled.div`
@@ -52,7 +58,7 @@ const AnimatedNav = () => {
   let location = useLocation();
 
   return (
-    <RootDiv style={styles.fill}>
+    <RootDiv>
       <Overlay>
         <RouteLink to="/about">
           <ArrowRight
@@ -65,69 +71,26 @@ const AnimatedNav = () => {
           />
         </RouteLink>
       </Overlay>
-      <div style={styles.content}>
+      <RouteDiv>
         <TransitionGroup>
           <CSSTransition
             key={location.key}
-            classNames="fade"
-            timeout={300}
+            classNames='pageSlider'
+            timeout={500}
+            mountOnEnter={false}
+            unmountOnExit
           >
-            <Switch location={location}>
-              <Route path="/about" children={<AboutPage />} />
-              <Route path="/" children={<Homepage />} />
-            </Switch>
+            <div className='pageSlider'>
+              <Switch location={location}>
+                <Route path="/about" children={<AboutPage />} />
+                <Route path="/" children={<Homepage />} />
+              </Switch>
+            </div>
           </CSSTransition>
         </TransitionGroup>
-      </div>
+      </RouteDiv>
     </RootDiv>
   );
 }
 
 export default AnimatedNav;
-
-const styles = {};
-
-styles.fill = {
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0
-};
-
-styles.content = {
-  ...styles.fill,
-  top: "40px",
-  textAlign: "center"
-};
-
-styles.nav = {
-  padding: 0,
-  margin: 0,
-  position: "absolute",
-  top: 0,
-  height: "40px",
-  width: "100%",
-  display: "flex"
-};
-
-styles.navItem = {
-  textAlign: "center",
-  flex: 1,
-  listStyleType: "none",
-  padding: "10px"
-};
-
-styles.hsl = {
-  ...styles.fill,
-  color: "white",
-  paddingTop: "20px",
-  fontSize: "30px"
-};
-
-styles.rgb = {
-  ...styles.fill,
-  color: "white",
-  paddingTop: "20px",
-  fontSize: "30px"
-};
